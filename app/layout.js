65900b71f5732fe819fb3cb4932af6e716798996
iroid2 <iradtu2@gmail.com>
2023-07-25 08:16:15 +0300

    Initial commit

---
+++ app/layout.js
@@ -0,0 +1,17 @@
+import "./globals.scss";
+import { Inter } from "next/font/google";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata = {
+  title: "Lato Clone",
+  description: "Inspired to to server You",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
---
+++ app/page.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function page() {
+  return (
+    <div className="container">
+      <h1>Home Page</h1>
+    </div>
+  );
+}
